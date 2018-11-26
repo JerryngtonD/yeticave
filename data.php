040d767fe7c9cb10a5ab2718e3ff1f10403948f7
jerryngton <jerryngtond@gmail.com>
2018-11-26 15:47:04 +0300

    some fixes have done after review

--- data.php
+++ data.php
@@ -3,52 +3,52 @@ $is_auth = (bool) rand(0, 1);
 $user_name = 'Евгений';
 $user_avatar = 'img/user.jpg';
 $categories = [
-  'promo__item--boards' => '<Доски и лыжи',
-  'promo__item--attachment' => 'Крепления',
-  'promo__item--boots' => 'Ботинки',
-  'promo__item--clothing' => 'Одежда',
-  'promo__item--tools' => 'Инструменты',
-  'promo__item--other' => 'Разное'
+  'boards' => '<Доски и лыжи',
+  'attachment' => 'Крепления',
+  'boots' => 'Ботинки',
+  'clothing' => 'Одежда',
+  'tools' => 'Инструменты',
+  'other' => 'Разное'
 ];
 $goods = [
   [
     'productName' => '2014 Rossignol District Snowboard',
-    'categorie' => 'Доски и лыжи',
+    'categorie' => $categories['boards'],
     'price' => 10999,
     'url' => 'img/lot-1.jpg',
     'description' => 'Cноуборд'
   ],
   [
     'productName' => 'DC Ply Mens 2016/2017 Snowboard',
-    'categorie' => 'Доски и лыжи',
+    'categorie' => $categories['boards'],
     'price' => 159999,
     'url' => 'img/lot-2.jpg',
     'description' => 'Cноуборд'
   ],
   [
     'productName' => 'Крепление Union Contact Pro 2015 года размер L/XL',
-    'categorie' => 'Крепления',
+    'categorie' => $categories['attachment'],
     'price' => 8000,
     'url' => 'img/lot-3.jpg',
     'description' => 'Крепление'
   ],
   [
     'productName' => 'Ботинки для сноуборда DC Mutiny Charocal',
-    'categorie' => 'Ботинки',
+    'categorie' => $categories['boots'],
     'price' => 10999,
     'url' => 'img/lot-4.jpg',
     'description' => 'Ботинки для сноуборда'
   ],
   [
     'productName' => 'Куртка для сноуборда DC Mutiny Charocal',
-    'categorie' => 'Ботинки',
+    'categorie' => $categories['clothing'],
     'price' => 7500,
     'url' => 'img/lot-5.jpg',
     'description' => 'Куртка для сноуборда'
   ],
   [
     'productName' => 'Маска Oakley Canopy',
-    'categorie' => 'Разное',
+    'categorie' => $categories['other'],
     'price' => 5400,
     'url' => 'img/lot-6.jpg',
     'description' => 'Маска для сноуборда'
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-date_default_timezone_set("Europe/Moscow");
-
+require_once('conf.php');
 require_once('functions.php');
 require_once('data.php');
 
